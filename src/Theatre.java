cbfa97a1977a68c124212149c929ef8a49a51c95
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-14 22:43:28 +0530

    Program terminates when 0 is selected as the option.

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,9 +1,14 @@
+import java.util.*;
+
 public class Theatre {
     public static String [] row_1 = {"O","O","O","O","O","O","O","O","O","O","O","O"};
     public static String [] row_2 = {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"};
     public static String [] row_3 = {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"};
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to the New Theatre !");
 
         System.out.println("""
@@ -28,6 +33,13 @@ public class Theatre {
 
                       0) Quit.
                 """);
+
+        int option = 0;
+        while(true){
+            System.out.print("Enter your option: ");
+            option = input.nextInt();
+            break;
+        }
 
     }
 }
