141649237031bbbab5538190c79808d9954cf565
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-14 22:35:06 +0530

    Added the menu.

--- src/Theatre.java
+++ src/Theatre.java
@@ -5,5 +5,29 @@ public class Theatre {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the New Theatre !");
+
+        System.out.println("""
+                Please select an option:
+
+
+                1) Buy a ticket.
+
+                2) Print seating area.
+
+                3) Cancel ticket.
+
+                4) List available seats.
+
+                5) Save to file.
+
+                6) Load from file.
+
+                7) Print ticket information and total price.
+
+                8) Sort tickets by price.
+
+                      0) Quit.
+                """);
+
     }
 }
