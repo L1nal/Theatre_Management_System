594ae64c9957284750b0bbef55c877f576b06839
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-14 22:31:54 +0530

    Created the 3 arrays one for each row.

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,4 +1,8 @@
 public class Theatre {
+    public static String [] row_1 = {"O","O","O","O","O","O","O","O","O","O","O","O"};
+    public static String [] row_2 = {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"};
+    public static String [] row_3 = {"O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"};
+
     public static void main(String[] args) {
         System.out.println("Welcome to the New Theatre !");
     }
