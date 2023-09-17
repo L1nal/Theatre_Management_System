95bf370438908489b5a6cd7b898794ca29bc48a1
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-17 22:12:42 +0530

    Completed the buy_ticket method working fine, added the buyTicket method to verify the seat occupancy.

--- src/Theatre.java
+++ src/Theatre.java
@@ -76,6 +76,7 @@ public class Theatre {
         }
     }
 
+
     public static int inputData_checker(int min,int max) {
         Scanner input = new Scanner(System.in);
         int data;
@@ -96,14 +97,34 @@ public class Theatre {
         }
     }
 
+    public static void buyTicket_dataCheck(int min, int max, String arr[], float price, int row){
+        int seat_no = inputData_checker(min, max);
+        if (arr[seat_no - 1].equals("X")) {
+            System.out.println("This Seat is already occupied.");
+        }else if (arr[seat_no-1].equals("O")) {
+            arr[seat_no-1] = "X";
+            System.out.println("Seat reserved successfully.");
+        }
+    }
+
     public static void buy_ticket(){
 
         System.out.println("Enter the Row and Column Number of the seat you wish to reserve.");
 
         while(true) {
-            try{
+            try {
                 System.out.print("Enter the row number:");
                 int row_no = inputData_checker(1,3);
+                System.out.print("Enter the Seat number:");
+                if (row_no == 1) {
+                    buyTicket_dataCheck(1, 12, row_1, 10, 1);
+                } else if (row_no == 2) {
+                    buyTicket_dataCheck(1, 16, row_2, 20, 2);
+                } else if (row_no == 3) {
+                    buyTicket_dataCheck(1, 20, row_3, 30, 3);
+                }
+                break;
+
             } catch (Exception e) {
                 System.out.println("\n");
                 System.out.println("Invalid Input. Please try again.");
