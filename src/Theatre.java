6e21f2c0a50ddcb4876b19f68baee3175033b612
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-17 21:58:04 +0530

    Initiated the buy_ticket method and added the method inputData_checker to verify user inputs.

--- src/Theatre.java
+++ src/Theatre.java
@@ -73,33 +73,41 @@ public class Theatre {
                 default:
                     System.out.println("Enter a valid input.");
             }
-
         }
     }
 
-    public static int buy_ticket(){
-
+    public static int inputData_checker(int min,int max) {
         Scanner input = new Scanner(System.in);
+        int data;
+        while (true) {
+            try {
+                data = input.nextInt();
+                if (min <= data && data <= max) {
+                    return data;
+                } else {
+                    System.out.println("Out of range");
+                    System.out.println();
+                    System.out.print("Please enter a valid integer between "+ min +" - "+ max +" :");
+                }
+            } catch (InputMismatchException e) {
+                System.out.print("Please enter a valid integer");
+                input.nextInt();
+            }
+        }
+    }
+
+    public static void buy_ticket(){
 
         System.out.println("Enter the Row and Column Number of the seat you wish to reserve.");
 
         while(true) {
-            try {
+            try{
                 System.out.print("Enter the row number:");
-                int row_no = input.nextInt();
-                if (1 <= row_no && row_no <= 3) {
-                    return row_no;
-                } else {
-                    System.out.println("Out of range");
-                    System.out.println();
-                    System.out.print("Please enter a valid integer between " + 1 + " - " + 3 + " :");
-                    System.out.print("Enter the row number:");
-                }
+                int row_no = inputData_checker(1,3);
             } catch (Exception e) {
                 System.out.println("\n");
                 System.out.println("Invalid Input. Please try again.");
             }
         }
-
     }
 }
