f022e9456ddb0cc2176855c6de0c66b3e7fda5b8
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-19 11:53:53 +0530

    Created a new class Person.java constructor person takes th 3 variables as input to create an object Person.

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    private String name;
+    private String surname;
+    private String email;
+
+    public Person(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+}
