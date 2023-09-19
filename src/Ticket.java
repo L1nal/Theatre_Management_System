4a9706a92e474eab6692f71287d987487140c660
Linal Mathila Bandara <linalmathilab@gmail.com>
2023-09-19 11:59:51 +0530

    Created a new class Ticket.java.

---
+++ src/Ticket.java
@@ -0,0 +1,13 @@
+public class Ticket {
+    private int row;
+    private int seat;
+    private double price;
+    private Person person;
+
+    public Ticket(int row, int seat, double price, Person person) {
+        this.row = row;
+        this.seat = seat;
+        this.price = price;
+        this.person = person;
+    }
+}
